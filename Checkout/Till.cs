a721bea8694eacdef562d8964a849416d55cee8e
Shunfan <shishunfan@gmail.com>
2020-10-21 17:29:24 +1300

    improved

--- Checkout/Till.cs
+++ Checkout/Till.cs
@@ -38,40 +38,33 @@ namespace Checkout
                 // Variables of price and quantity
                 var itemPrice = _itemPrice[item.Key];
                 var itemNum = item.Value;
-                // Warn if an item exceeds max number
-                if(_itemsMaxAmount.ContainsKey(item.Key) && item.Value > _itemsMaxAmount[item.Key]) Console.WriteLine("{0} exceeds the maximum", item.Key);
-                // When the item has special condition and the quantity reaches the minimum requirement
+                // When the item has special condition and the quantity reaches the minimum requirement, caculate the special cost
                 if(_specialSets.ContainsKey(item.Key) && item.Value >= _specialSets[item.Key].Item1){  
                     // The tuple of the special condition for the item 
                     var specialSet = _specialSets[item.Key] ;
-                    // Add cost for items with special
-                    total = AddItemCostInSpecial(total, itemNum, itemPrice, specialSet);
+                    // Call the fuction to add cost for items with special
+                    total += ItemCostInSpecial(itemNum, itemPrice, specialSet);
                 }else {
                     // Add cost for items with no special
-                    total = AddItemCost(total, itemNum, itemPrice);
+                    total += itemPrice * itemNum;             
                 };    
             }  
            return total;
         
         }
-        // Item cost with special condition
-        private static double AddItemCostInSpecial(double total, int itemNum,int itemPrice,  Tuple<int, int> special)
+        //Caculate item cost with special condition
+        private static double ItemCostInSpecial(int itemNum,int itemPrice,  Tuple<int, int> special)
         {   
             //caculate the number of discounted items 
             var numberInSpecial =  itemNum / special.Item1;
             var numberNotInSpecial =  itemNum % special.Item1; 
             var specialSetPrice = special.Item2;
 
-            total += specialSetPrice * numberInSpecial + itemPrice * numberNotInSpecial;   
-            return total;
+            var cost = specialSetPrice * numberInSpecial + itemPrice * numberNotInSpecial;   
+            return cost;
         }
-        // Item cost with normal condition
-         private static double AddItemCost(double total, int itemNum,int itemPrice)
-        {   
-            total += itemPrice * itemNum;    
-            return total;
-        }
-
+      
+        //Add item number exceed warn
         public void Scan(string items)
         {
             //Upper the lower cases
@@ -79,6 +72,8 @@ namespace Checkout
             foreach(var item in items)
             {
                 _items[item]++;  
+                // Warn if an item exceeds max number
+                if(_itemsMaxAmount.ContainsKey(item) && _items[item] > _itemsMaxAmount[item]) Console.WriteLine("{0} exceeds the maximum", item);              
             }
         }
     }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Changes:
-1. Calculate special items and ordinary items separately through two functions
+1. Calculate the cost of special items through one function
 2. Add multiple dictionaries to make the data easy to expand. When the number of products increases (or increase promotion conditions, limit the max number of more products), only need to add data to the corresponding dictionary without changing the function
 3. The naming is simple and easy to understand, increasing code readability
 4. Optimized code logic, more concise
